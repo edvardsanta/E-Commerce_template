e1400cf836f78447d003be55bf26ead7a82315dc
Edvard <75655486+edvardsanta@users.noreply.github.com>
2022-03-21 16:17:19 -0300

    SRC changed

--- src/routes/order.js
+++
@@ -1,7 +0,0 @@
-import Express from "express";
-import Order from "../models/Order.js";
-import {verifyToken, verifyToken_Authorization, verifyToken_Admin, } from "./vToken.js";
-const route = Express.Router();
-
-
-export default route;
