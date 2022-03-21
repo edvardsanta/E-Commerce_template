8647c266ca6725f77ff5dc2c74aee05ac67c1206
eduardo <edu@localhost.localdomain>
2022-03-21 18:58:55 -0300

    Teste do frontend

---
+++ frontend/src/pages/main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import styles from './styles';
+
+const Main = () => (
+  <View>
+    <Text> Página principal </Text>
+  </View>
+);
+
+export default Main;
---
+++ frontend/src/routes.js
@@ -0,0 +1,11 @@
+import { createStackNavigator } from 'react-navigation-stack';
+
+//import SignIn from './pages/signin';
+import Main from './pages/main';
+
+const Routes = createStackNavigator({
+  //SignIn,
+  Main,
+});
+
+export default Routes;
